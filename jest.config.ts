f6931385f12fd12b2f7c0172faa58f3d8c74e720
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-05-22 20:34:55 +0300

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^.+\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
   }
 };
 
