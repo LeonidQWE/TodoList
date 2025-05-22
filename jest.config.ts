96c15698209e031d69f894b11c23a51153a32e28
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-05-22 14:41:06 +0300

    add test config

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/internal/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': "ts-jest"
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  }
+};
+
+export default config;
