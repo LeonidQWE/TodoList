8a61cd44c57084c8b7602c27bf20167a3fa076f6
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-05-29 17:04:30 +0300

    fix imports in TodoForm

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Title } from './title/Title';
 export { Main } from './main/Main';
 export { TodoForm } from './todoForm/TodoForm';
+export { CommonField } from './commonField/CommonField';
