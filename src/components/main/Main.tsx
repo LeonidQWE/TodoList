c6da349ec57d79e4834854039ca852ab2f7f416b
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2025-05-30 18:58:42 +0300

    update Main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,29 @@
 import { useTodo } from 'hooks/useTodo';
-import { Title, TodoForm, TodoList } from 'components';
+import { Title, TodoForm, TodoList, TodosActions } from 'components';
 import s from './Main.module.scss';
 
 export const Main = () => {
-  const { todos, addTodo, deleteTodo, toggleTodo} = useTodo();
+  const {
+    filteredTodos,
+    completedTodosCount,
+    addTodo,
+    deleteTodo,
+    toggleTodo,
+    deleteAllTodos,
+    resetCompletedTodos,
+    setFilter,
+  } = useTodo();
 
   return (
     <div className={s.main}>
       <Title level={1} text={'Todo List'}/>
       <TodoForm addTodo={addTodo}/>
-      <TodoList todos={todos} deleteTodo={deleteTodo} toggleCompleteTodo={toggleTodo}/>
+      <TodosActions
+        deleteAllTodos={deleteAllTodos}
+        resetCompletedTodos={resetCompletedTodos}
+        setFilter={setFilter}
+        completedTodosExist={!!completedTodosCount}/>
+      <TodoList todos={filteredTodos} deleteTodo={deleteTodo} toggleCompleteTodo={toggleTodo}/>
     </div>
   );
 };
